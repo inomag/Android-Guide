e9359b94407f0992c6ae6128bcaa62f20c6586f0
Inomag <pratiksr12345@gmail.com>
2021-04-30 00:30:56 +0530

    Map Dialog

--- Android Components/app/src/main/java/com/example/androidcomponents/MainActivity.java
+++ Android Components/app/src/main/java/com/example/androidcomponents/MainActivity.java
@@ -2,13 +2,79 @@ package com.example.androidcomponents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MaterialButton mapDialogBtn,markLocBtn;
+    private ImageButton getCurrentLoc;
+    private MapView map;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        mapDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(v.getContext());
+                dialog.setContentView(R.layout.map_dialog);
+                dialog.setCancelable(true);
+                WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                lp.copyFrom(dialog.getWindow().getAttributes());
+                lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+                lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+                map = dialog.findViewById(R.id.dialog_map);
+                map.onCreate(savedInstanceState);
+                map.getMapAsync(MainActivity.this::onMapReady);
+
+                markLocBtn = dialog.findViewById(R.id.mark_loc_btn);
+                getCurrentLoc = dialog.findViewById(R.id.get_current_loc);
+
+                markLocBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+                dialog.getWindow().setAttributes(lp);
+
+            }
+        });
+    }
+
+    private void initViews() {
+        mapDialogBtn = findViewById(R.id.map_dialog_btn);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+        mMap.getUiSettings().isMyLocationButtonEnabled();
+        LatLng position = new LatLng(27.231775, 94.104646);
+        mMap.addMarker(new MarkerOptions().position(position));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
     }
 }
