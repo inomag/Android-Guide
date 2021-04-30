398a07d8359eb9660794ded33078e7e0b43092a2
Inomag <pratiksr12345@gmail.com>
2021-04-30 23:57:21 +0530

    Current Location Service Done

--- Android Components/app/src/main/java/com/example/androidcomponents/ui/MapDialog.java
+++ Android Components/app/src/main/java/com/example/androidcomponents/ui/MapDialog.java
@@ -1,38 +1,87 @@
 package com.example.androidcomponents.ui;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.androidcomponents.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 public class MapDialog extends DialogFragment implements OnMapReadyCallback {
 
     View root;
     private MapView map;
     GoogleMap mGoogleMap;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    MaterialButton markLocation;
+    AppCompatImageButton getCurrentLocation;
+    private Context mContext;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         root = inflater.inflate(R.layout.map_dialog,container,false);
+        mContext = getActivity().getApplicationContext();
+        markLocation = root.findViewById(R.id.mark_loc_btn);
+        getCurrentLocation = root.findViewById(R.id.get_current_loc);
+        getCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED&&ActivityCompat.checkSelfPermission(mContext,Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                    getCoordinates();
+                }else{
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION
+                    },100);
+                }
+            }
+        });
+
         return root;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==100 && grantResults.length>0 && (grantResults[0]+grantResults[1]==PackageManager.PERMISSION_GRANTED)){
+            getCoordinates();
+        }else{
+            Toast.makeText(mContext, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -48,16 +97,61 @@ public class MapDialog extends DialogFragment implements OnMapReadyCallback {
             fm.executePendingTransactions();
         }
         mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity().getApplicationContext());
+
     }
 
+    @SuppressLint("MissingPermission")
     private void getCoordinates() {
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if(location!=null){
+                        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                        mGoogleMap.clear();
+                        mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                    }else{
+                        LocationRequest locationRequest = new LocationRequest()
+                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                .setInterval(10000)
+                                .setFastestInterval(1000)
+                                .setNumUpdates(1);
+                        LocationCallback locationCallback = new LocationCallback(){
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                Location location1 = locationResult.getLastLocation();
+                                LatLng latLng = new LatLng(location1.getLatitude(),location1.getLongitude());
+                                mGoogleMap.clear();
+                                mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+                                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                            }
+                        };
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
+                    }
+                }
+            });
+        }else{
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        LatLng position = new LatLng(27.231775, 94.104646);
-        mGoogleMap.addMarker(new MarkerOptions().position(position));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mGoogleMap.clear();
+                mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+            }
+        });
+
     }
 }
